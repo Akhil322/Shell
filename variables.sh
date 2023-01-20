90a053d2f94bb90050db549f7e4ef2315587b4d8
Akhil322 <eakhilraj1997@gmail.com>
2023-01-20 18:55:11 +0530

    first commit using intellij

--- variables.sh
+++ variables.sh
@@ -6,5 +6,17 @@ echo "welcome good morning today date is 20-01-2023"
 # command substitution
 #var = $((command))
 
+#date +'FORMAT'
+
+### mm/dd/yyyy ###
+date +'%m/%d/%Y'
+
+## Time in 12 hr format ###
+date +'%r'
+
+## backup dir format ##
+backup_dir=$(date +'%m/%d/%Y')
+echo "Backup dir for today: /nas04/backups/${backup_dir}"
+
 DATE = $(date +%F)
 echo "welcome good morning today date is $DATE"
